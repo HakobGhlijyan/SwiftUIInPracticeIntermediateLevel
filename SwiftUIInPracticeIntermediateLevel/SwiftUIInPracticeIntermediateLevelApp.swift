849b5419c162cf9f6e4d087e8d535083b5bc58b1
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-03-29 16:35:23 +0400

    Initial Commit

---
+++ SwiftUIInPracticeIntermediateLevel/SwiftUIInPracticeIntermediateLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInPracticeIntermediateLevelApp.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInPracticeIntermediateLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
