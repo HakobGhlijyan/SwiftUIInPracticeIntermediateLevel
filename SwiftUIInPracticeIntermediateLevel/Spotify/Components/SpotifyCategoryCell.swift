f7b7243c282ff0295e39543aaff9b0a5eaa4175c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-03-30 17:41:49 +0400

    Practice 02 End

---
+++ SwiftUIInPracticeIntermediateLevel/Spotify/Components/SpotifyCategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyCategoryCell.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 30.03.2024.
+//
+
+import SwiftUI
+
+struct SpotifyCategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyCategoryCell()
+}
---
+++ SwiftUIInPracticeIntermediateLevel/Spotify/Models/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 30.03.2024.
+//
+
+import Foundation
