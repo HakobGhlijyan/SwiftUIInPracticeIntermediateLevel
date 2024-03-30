4ccf70f3c000cd3f0f8c6771777f9521833addf7
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-03-30 20:44:41 +0400

    Practice 03 End

---
+++ SwiftUIInPracticeIntermediateLevel/Spotify/Components/SpotifyNewReleaseCell.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyNewReleaseCell.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 30.03.2024.
+//
+
+import SwiftUI
+
+struct SpotifyNewReleaseCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyNewReleaseCell()
+}
