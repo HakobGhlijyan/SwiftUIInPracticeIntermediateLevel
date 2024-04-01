e20a580f074bb04fefb962ac8628987b8901c3c3
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-04-01 20:22:41 +0400

    Practice 06 End

---
+++ SwiftUIInPracticeIntermediateLevel/Spotify/Components/SongRowCell.swift
@@ -0,0 +1,18 @@
+//
+//  SongRowCell.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 01.04.2024.
+//
+
+import SwiftUI
+
+struct SongRowCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongRowCell()
+}
