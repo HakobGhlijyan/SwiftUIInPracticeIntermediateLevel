5199c3c61819cdbced81c1ea70aaf461351e22cc
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-04-01 11:38:04 +0400

    Practice 4 End

---
+++ SwiftUIInPracticeIntermediateLevel/Spotify/Components/ImageTitleRowCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageTitleRowCell.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 31.03.2024.
+//
+
+import SwiftUI
+
+struct ImageTitleRowCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageTitleRowCell()
+}
