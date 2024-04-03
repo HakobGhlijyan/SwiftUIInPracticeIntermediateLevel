e36690639474fce29e7442ba46693f18e7b56d6f
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-04-03 17:09:29 +0400

    Practice 07 End. Bumble part 1

---
+++ SwiftUIInPracticeIntermediateLevel/Bumble/Core/BumbleHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  BumbleHomeView.swift
+//  SwiftUIInPracticeIntermediateLevel
+//
+//  Created by Hakob Ghlijyan on 03.04.2024.
+//
+
+import SwiftUI
+
+struct BumbleHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BumbleHomeView()
+}
