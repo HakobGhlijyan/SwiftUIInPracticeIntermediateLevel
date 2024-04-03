5ba8653f966351d716f5bc1a834d9d1e0345fc17
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-04-03 17:09:55 +0400

    Practice 07 End. Bumble part 2. Start

--- SwiftUIInPracticeIntermediateLevel/Bumble/Components/BumbleFilterView.swift
+++ SwiftUIInPracticeIntermediateLevel/Bumble/Components/BumbleFilterView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct BumbleFilterView: View {
+    
+    var options: [String] = ["Everyone", "Trending"]
+    @Binding var selection: String
+    @Namespace private var namespace
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top, spacing: 32) {
+            ForEach(options, id: \.self) { option in
+                VStack(spacing: 8.0) {
+                    Text(option)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                    if selection == option {
+                        RoundedRectangle(cornerRadius: 2)
+                            .frame(height: 2)
+                            .matchedGeometryEffect(id: "rectengle", in: namespace)
+                    }
+                }
+                .padding(.top, 8)
+                .background(.black.opacity(0.001))
+                .foregroundStyle(selection == option ? .bumbleBlack : .bumbleGray)
+                .onTapGesture { selection = option }
+            }
+        }
+        .animation(.smooth, value: selection)
+    }
+}
+
+fileprivate struct BumbleFilterViewPreview: View {
+    
+    var options: [String] = ["Everyone", "Trending", "All"]
+    @State private var selection: String = "Everyone"
+    
+    var body: some View {
+        BumbleFilterView(options: options, selection: $selection)
     }
 }
 
 #Preview {
-    BumbleFilterView()
+    BumbleFilterViewPreview()
+        .padding()
 }
--- SwiftUIInPracticeIntermediateLevel/Bumble/Core/BumbleHomeView.swift
+++ SwiftUIInPracticeIntermediateLevel/Bumble/Core/BumbleHomeView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct BumbleHomeView: View {
+    
+    @State private var filters: [String] = ["Everyone", "Trending"]
+    @AppStorage("bumble_home_filter") private var selectedFilter: String = "Everyone"
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //1
+            Color.bumbleWhite.ignoresSafeArea()
+            
+            //2
+            VStack(spacing: 12) {
+                header
+                BumbleFilterView(options: filters, selection: $selectedFilter)
+                    .background(Divider(), alignment: .bottom)
+                
+                Spacer()
+            }
+            .padding(8)
+        }
     }
 }
 
 #Preview {
     BumbleHomeView()
+}
+
+//some View
+extension BumbleHomeView {
+    private var header: some View {
+        HStack(spacing: 0.0) {
+            //1
+            HStack(spacing: 0.0) {
+                Image(systemName: "line.horizontal.3")
+                    .padding(8)
+                    .background(.black.opacity(0))
+                    .onTapGesture {    }
+                Image(systemName: "arrow.uturn.left")
+                    .padding(8)
+                    .background(.black.opacity(0))
+                    .onTapGesture {    }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            //2
+            Text("bumble")
+                .font(.title)
+                .foregroundStyle(.bumbleYellow)
+                .frame(maxWidth: .infinity, alignment: .center)
+            //3
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(.black.opacity(0))
+                .onTapGesture {    }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumbleBlack)
+    }
 }
