5f939467f47b08144de9ae3ff49ec40822b463cf
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-04-04 19:52:14 +0400

    Practice 09 Clean code

--- SwiftUIInPracticeIntermediateLevel/Bumble/Core/BumbleHomeView.swift
+++ SwiftUIInPracticeIntermediateLevel/Bumble/Core/BumbleHomeView.swift
@@ -11,12 +11,8 @@ import SwiftfulUI
 struct BumbleHomeView: View {
     @State private var filters: [String] = ["Everyone", "Trending"]
     @AppStorage("bumble_home_filter") private var selectedFilter: String = "Everyone"
-    ///sozdadim masiv gde budut vse useri ... kotorie budut pokazivatsya na ui vmesto red rec
     @State private var allUser:[User] = []
-    /// eto budet nash tekuchiy index user profil.. kotoriy mi budem delat +1 chtob prereyti na sleduchiy user
     @State private var selectedIndex: Int = 0
-    /// eto budet tekuchee znachenie card , on ssmechen na levo ili na pravo ... v vide slovarya [:] - eto pustoy, INt potomu chto id u user v Int vide
-    /// User - > id Int -> direction R -> true , L -> false
     @State private var cardOffsets: [Int:Bool] = [:]
     @State private var currentSwipeOffset: CGFloat = 0
 
@@ -27,109 +23,31 @@ struct BumbleHomeView: View {
             Color.bumbleWhite.ignoresSafeArea()
             //2
             VStack(spacing: 12) {
-                header                                                          //1
-                BumbleFilterView(options: filters, selection: $selectedFilter)  //2
-                    .background(Divider(), alignment: .bottom)                  //3
+                header
+                BumbleFilterView(options: filters, selection: $selectedFilter)
+                    .background(Divider(), alignment: .bottom)
                 
                 ZStack {
                     if !allUser.isEmpty {
                         ForEach(Array(allUser.enumerated()), id: \.offset) { (index , user) in
-                            //1
-                            ///enumerated , dlya togo chtob poluchit posledovotelnost userov
-                            ///no ego nado v Array , offset chtob poluchit ix smeshenie
-                            ///on nam dayot .. -> 2 obekta  index i user
-//                            Rectangle()
-//                                .fill(.red)
-//                                .overlay( Text("\(index)") )
-                            /// pokazivaet  0 v samom nizu a 29 na samom verxu
-                            //2
-                            /// tak mi xotim chtob pokazivalos tekuchiy index ... esli current index = index , to on pokajet ego
-                            /// no nam na ekrane nujno ne vse user a tolko te kto seychat , potom i bili do
-                            let isPrevious = (selectedIndex - 1) == index    // prediduchiy
-                            let isCurrent = selectedIndex == index           // tekuchiy
-                            let isNext = (selectedIndex + 1) == index        // sleduychiy
+                            let isPrevious = (selectedIndex - 1) == index
+                            let isCurrent = selectedIndex == index
+                            let isNext = (selectedIndex + 1) == index
                             
                             if isPrevious || isCurrent || isNext {
-                                /// nash offset , kotoriy dudet naznachtsya na true ili false
                                 let offsetValue = cardOffsets[user.id]
-                                
-                                //1
-                                /*
-                                 Rectangle()
-                                     .fill(index == 0 ? .red : .blue)
-                                     .overlay( Text("\(index)") )
-                                     .withDragGesture(
-                                         .horizontal,                   //vid peretaskivaniya
- //                                        minimumDistance: 0,
-                                         resets: true,                  //vid vozrochaetsya nazad -> true
-                                         rotationMultiplier: 1.05,      //pri smechenii budet povorot
-                                         onChanged: { dragOffset in     //pri peremechenii
-                                             
-                                         },
-                                         onEnded: { dragOffset in       //pri okonchanii pereecheniya
-                                             // zdesmi naznachim v offset znachenie na kotoroe on poydet na R L
- //                                            offset = dragOffset.width  // -50 ili 50
-                                             
-                                             if dragOffset.width < -50 {    // esli on budet menche -50
-                                                 userDidSelect(index: index, isLike: false)  // to on delaet X No
-                                             } else if dragOffset.width > 50 {
-                                                 userDidSelect(index: index, isLike: true)  // to on delaet V Yes
-                                             }
-                                         }
-                                     )
-                                 */
-                                userProfileCell(user: user, index: index)   //teper grag gester imeet prioritet...->
+                                userProfileCell(user: user, index: index)
                                     .zIndex(Double(allUser.count - index))
-                                ///kajdomu prisvoivaem svoy index , naprimenr -> elis tam 100 - 0 = 100
-//                                    .offset(x: -900)   /// on smestit na pravo ili na levo to kotoriy viden
-                                /// sdes mi smotrim offsetValue raven  nil , to mi ne smaxivaem , a esli on true to 900 v pravo , false na levo
                                     .offset(x: offsetValue == nil ? 0 : offsetValue == true ? 900 : -900)
                             }
-                            
                         }
                     } else {
                         ProgressView()
                     }
-                    
-                    //2 etot sloy bdet dlya indicatorov X V  . rec budet no ne viden... po umolchaniyu on z index 1 , dlya etog mi naznachim 999999 , chtob vien bil
-                    //1v
-                    /*
-                     ZStack {
-                         // 1 - xmark
-                         Circle()
-                             .fill(.bumbleGray.opacity(0.4))
-                             .overlay(
-                             Image(systemName: "xmark")
-                                 .font(.title)
-                                 .fontWeight(.semibold)
-                             )
-                             .frame(width: 60, height: 60)
-                             .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)  // absolut znachenie ono - ili + .. on vzmet i zdelaet ego nemnogo bolche, pri smechenii na 100 on 1,5 maschtab a esli net to on budet togo je razmera.
-                             .offset(x: min(-currentSwipeOffset, 150))  //delaem minimalniy 150 , posle nego on ne dvinetsya
-                             .offset(x: -100)
-                             .frame(maxWidth: .infinity, alignment: .leading)
-                         //2
-                         Circle()
-                             .fill(.bumbleGray.opacity(0.4))
-                             .overlay(
-                             Image(systemName: "checkmark")
-                                 .font(.title)
-                                 .fontWeight(.semibold)
-                             )
-                             .frame(width: 60, height: 60)
-                             .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)  // absolut znachenie ono - ili + .. on vzmet i zdelaet ego nemnogo bolche, pri smechenii na 100 on 1,5 maschtab a esli net to on budet togo je razmera.
-                             .offset(x: max(-currentSwipeOffset, -150))  //delaem minimalniy 150 , posle nego on ne dvinetsya
-                             .offset(x: 100)
-                             .frame(maxWidth: .infinity, alignment: .trailing)
-                     }
-                     .zIndex(9999999)
-                     */
-                    //2v
                     overlaySwipingIndicator
                         .zIndex(9999999)
                 }
-                .frame(maxHeight: .infinity) ///tak nach frame budet , daje esli tam nichego net
-                ///dobavi animation chtob etot perexod bil plavnee , animu=iruem cardOffsets smenu etogo znacheniya
+                .frame(maxHeight: .infinity)
                 .padding(4)
                 .animation(.smooth, value: cardOffsets)
             }
@@ -138,7 +56,7 @@ struct BumbleHomeView: View {
         .task {
             await getData()
         }
-        .toolbar(.hidden, for: .navigationBar) ///chtob on v stack bil skrit
+        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
@@ -146,7 +64,6 @@ struct BumbleHomeView: View {
         BumbleHomeView()
 }
 
-//some View
 extension BumbleHomeView {
     private var header: some View {
         HStack(spacing: 0.0) {
@@ -182,66 +99,32 @@ extension BumbleHomeView {
         .fontWeight(.medium)
         .foregroundStyle(.bumbleBlack)
     }
-    
-    //1v
-    /*
-     private func userProfileCell(index: Int) -> some View {
-         Rectangle()
-             .fill(index == 0 ? .red : .blue)
-             .overlay( Text("\(currentSwipeOffset)").foregroundStyle(.white) )
-             .withDragGesture(
-                 .horizontal,                   //vid peretaskivaniya
-                 //                                        minimumDistance: 0,
-                 resets: true,                  //vid vozrochaetsya nazad -> true
-                 rotationMultiplier: 1.05,      //pri smechenii budet povorot
-                 onChanged: { dragOffset in     //pri peremechenii
-                     //
-                     currentSwipeOffset = dragOffset.width
-                 },
-                 onEnded: { dragOffset in       //pri okonchanii pereecheniya
-                     // zdesmi naznachim v offset znachenie na kotoroe on poydet na R L
-                     //                                            offset = dragOffset.width  // -50 ili 50
-                     
-                     if dragOffset.width < -50 {    // esli on budet menche -50
-                         userDidSelect(index: index, isLike: false)  // to on delaet X No
-                     } else if dragOffset.width > 50 {
-                         userDidSelect(index: index, isLike: true)  // to on delaet V Yes
-                     }
-                 }
-             )
-     }
-     */
-    //v2
     private func userProfileCell(user: User, index: Int) -> some View {
         BumbleCardView(
-            user: user,    // peredadim user
+            user: user,
             onSendAComplimentPresed: nil,
             onSuperLikePresed: nil,
-            onXmarkLikePresed: {    // eto budet toje samoe chto i smaxivanie - > false
+            onXmarkLikePresed: {
                 userDidSelect(index: index, isLike: false)
             },
-            onCheckmarkPresed: {    // eto budet toje samoe chto i smaxivanie - > true
+            onCheckmarkPresed: {
                 userDidSelect(index: index, isLike: true)
             },
             onHideAndRaportPresed: nil
         )
         .withDragGesture(
-            .horizontal,                   //vid peretaskivaniya
-            minimumDistance: 20,          //teper grag gester imeet prioritet...-> on ne dayot scrol view rabotat, takim , nujno sdelat min distanse naprimer 10 .. chtob on znal kogda rabotal draggesture a congda scroll v view, tak chto on esli sdvinul minium menche > 10 v levo iliv pravo, to on ne budet rabotat drag , a scroll zarabotaet. 0 ne rabotaet , a 20 zarabtaet
-            resets: true,                  //vid vozrochaetsya nazad -> true
-            rotationMultiplier: 1.05,      //pri smechenii budet povorot
-            onChanged: { dragOffset in     //pri peremechenii
-                //
+            .horizontal,
+            minimumDistance: 20,
+            resets: true,
+            rotationMultiplier: 1.05,
+            onChanged: { dragOffset in
                 currentSwipeOffset = dragOffset.width
             },
-            onEnded: { dragOffset in       //pri okonchanii pereecheniya
-                // zdesmi naznachim v offset znachenie na kotoroe on poydet na R L
-                //                                            offset = dragOffset.width  // -50 ili 50
-                
-                if dragOffset.width < -50 {    // esli on budet menche -50
-                    userDidSelect(index: index, isLike: false)  // to on delaet X No
+            onEnded: { dragOffset in
+                if dragOffset.width < -50 {
+                    userDidSelect(index: index, isLike: false)
                 } else if dragOffset.width > 50 {
-                    userDidSelect(index: index, isLike: true)  // to on delaet V Yes
+                    userDidSelect(index: index, isLike: true)
                 }
             }
         )
@@ -249,7 +132,6 @@ extension BumbleHomeView {
     
     private var overlaySwipingIndicator: some View {
         ZStack {
-            // 1 - xmark
             Circle()
                 .fill(.bumbleGray.opacity(0.4))
                 .overlay(
@@ -258,11 +140,10 @@ extension BumbleHomeView {
                     .fontWeight(.semibold)
                 )
                 .frame(width: 60, height: 60)
-                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)  // absolut znachenie ono - ili + .. on vzmet i zdelaet ego nemnogo bolche, pri smechenii na 100 on 1,5 maschtab a esli net to on budet togo je razmera.
-                .offset(x: min(-currentSwipeOffset, 150))  //delaem minimalniy 150 , posle nego on ne dvinetsya
+                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)
+                .offset(x: min(-currentSwipeOffset, 150))
                 .offset(x: -100)
                 .frame(maxWidth: .infinity, alignment: .leading)
-            //2
             Circle()
                 .fill(.bumbleGray.opacity(0.4))
                 .overlay(
@@ -271,8 +152,8 @@ extension BumbleHomeView {
                     .fontWeight(.semibold)
                 )
                 .frame(width: 60, height: 60)
-                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)  // absolut znachenie ono - ili + .. on vzmet i zdelaet ego nemnogo bolche, pri smechenii na 100 on 1,5 maschtab a esli net to on budet togo je razmera.
-                .offset(x: max(-currentSwipeOffset, -150))  //delaem minimalniy 150 , posle nego on ne dvinetsya
+                .scaleEffect(abs(currentSwipeOffset) > 100 ? 1.5 : 1)
+                .offset(x: max(-currentSwipeOffset, -150))
                 .offset(x: 100)
                 .frame(maxWidth: .infinity, alignment: .trailing)
         }
@@ -280,10 +161,6 @@ extension BumbleHomeView {
     }
 }
 
-//Funcs
-/// u nas tak je budet func v kotoroy mi budem poluchat vsex user
-/// i onbudet vivan v .task ...
-/// v func mi snachalo proveryaem chto all user uje zagrujani . ili elsli net to delaem to chto posle guard a ->
 extension BumbleHomeView {
     private func getData() async {
         guard allUser.isEmpty else { return }
@@ -295,9 +172,8 @@ extension BumbleHomeView {
     }
     
     private func userDidSelect(index: Int, isLike: Bool) {
-        let user = allUser[index]                   // nash user kotoriy na ekrane.. ego index
-        cardOffsets[user.id] = isLike               // nash smechenie islike budet true to [user.id] peredastsya true
-        
-        selectedIndex += 1                          // etim mi skajem ne vajo chto bilo true ili false , emu pribavim +1 chtob pereyti na sleduchego user , to est pokazat next user... on stanet po seredine , a to kotoroe smaxnuli ili poka ostanetsya  , budet ili R ili L
+        let user = allUser[index]
+        cardOffsets[user.id] = isLike
+        selectedIndex += 1
     }
 }
